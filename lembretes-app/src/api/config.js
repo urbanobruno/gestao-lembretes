024e8fadaf61428bd34750364c5c873b49272879
Bruno Urbano <bruno.urbano.rocha@gmail.com>
2023-08-26 15:19:01 -0300

    imagens + video

--- lembretes-app/src/api/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    DB_HOST: 'localhost',
-    DB_USER: 'root', // Digite seu usuário mysql
-    DB_PASSWORD: 'bruno123', // Digite sua senha de usuário
-    DB_DATABASE: 'lembretes_db',
-  };
